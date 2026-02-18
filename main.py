3128975a39fd75d228a3f7542cc1f3079b735cef
amiralinmp841-alt <amiralinmp841@gmail.com>
2026-02-19 02:13:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -351,7 +351,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data["current_node"] = "root"
 
     await update.message.reply_text(
-        "🕊️ به ربات دانشگاه خوش آمدید. (V_4.2.10🔥)",
+        "🕊️ به ربات دانشگاه خوش آمدید. (V_4.2.11🔥)",
         reply_markup=get_keyboard("root", is_admin)
     )
 
